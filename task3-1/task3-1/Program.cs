2d547e939db59a903c280eb8aadb3d4fcdfe1984
salma mohamed <salmamahmed091@gmail.com>
2025-08-22 01:06:20 +0300

    Add project files.

---
+++ task3-1/task3-1/Program.cs
@@ -0,0 +1,166 @@
+﻿namespace task3_1
+{
+    internal class Program
+    {
+       static List<int> mylist = new List<int>();
+        static void Main(string[] args)
+        {
+            string choice;
+            do
+            {
+                Menu();
+
+                choice = Console.ReadLine().ToLower();
+                switch (choice)
+                {
+                    case "p":
+                        PrintNumbers();
+                        break;
+                    case "a":
+                        AddNumber();
+                        break;
+                    case "m":
+                        DisplayMean();
+                        break;
+                    case "s":
+                        DisplaySmallest();
+                        break;
+                    case "l":
+                        DisplayLargest();
+                        break;
+                    case "f":
+                        FindNumber();
+                        break;
+                    case "c":
+                        ClearList();
+                        break;
+                    case "q":
+                        Console.WriteLine("Goodbye");
+                        break;
+                    default:
+                        Console.WriteLine("Unknown selection, please try again");
+                        break;
+                }
+
+
+            } while (choice != "q");
+        }
+        static void Menu()
+        {
+            Console.WriteLine("P - Print numbers");
+            Console.WriteLine("A - Add a number");
+            Console.WriteLine("M - Display mean of the numbers");
+            Console.WriteLine("S - Display the smallest number");
+            Console.WriteLine("L - Display the largest number");
+            Console.WriteLine("F - Find a number");
+            Console.WriteLine("C - Clear the list");
+            Console.WriteLine("Q - Quit");
+            Console.WriteLine("===============================");
+            Console.Write("Enter your choice: ");
+        }
+        static void PrintNumbers()
+        {
+            if (mylist.Count == 0)
+            {
+                Console.WriteLine("[] - the list is empty");
+            }
+            else
+            {
+                Console.Write("[");
+                for (int i = 0; i < mylist.Count; i++)
+                {
+                    Console.Write(mylist[i]);
+                    if (i < mylist.Count - 1)
+                        Console.Write(" ");
+                }
+                Console.WriteLine("]");
+            }
+        }
+        static void AddNumber()
+        {
+            Console.Write("Enter an integer to add: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            mylist.Add(num);
+            Console.WriteLine($"{num} added");
+        }
+        static void DisplayMean()
+        {
+            if (mylist.Count == 0)
+            {
+                Console.WriteLine("Unable to calculate the mean - no data");
+            }
+            else
+            {
+                int sum = 0;
+                for (int i = 0; i < mylist.Count; i++)
+                    sum += mylist[i];
+                double mean = (double)sum / mylist.Count;
+                Console.WriteLine(mean);
+            }
+        }
+
+        static void DisplaySmallest()
+        {
+            if (mylist.Count == 0)
+            {
+                Console.WriteLine("Unable to determine the smallest number - list is empty");
+            }
+            else
+            {
+                int smallest = mylist[0];
+                for (int i = 1; i < mylist.Count; i++)
+                    if (mylist[i] < smallest)
+                        smallest = mylist[i];
+                Console.WriteLine(smallest);
+            }
+        }
+        static void DisplayLargest()
+        {
+            if (mylist.Count == 0)
+            {
+                Console.WriteLine("Unable to determine the largest number - list is empty");
+            }
+            else
+            {
+                int largest = mylist[0];
+                for (int i = 1; i < mylist.Count; i++)
+                    if (mylist[i] > largest)
+                        largest = mylist[i];
+                Console.WriteLine(largest);
+            }
+        }
+        static void FindNumber()
+        {
+            if (mylist.Count == 0)
+            {
+                Console.WriteLine("List is empty");
+            }
+            else
+            {
+                Console.Write("Enter number to search: ");
+                int searchNum = Convert.ToInt32(Console.ReadLine());
+                bool found = false;
+                for (int i = 0; i < mylist.Count; i++)
+                {
+                    if (mylist[i] == searchNum)
+                    {
+                        Console.WriteLine($"{searchNum} found at index {i}");
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    Console.WriteLine($"{searchNum} not found");
+                }
+            }
+        }
+        static void ClearList()
+        {
+            mylist.Clear();
+            Console.WriteLine("List cleared");
+        }
+
+
+    }
+}
---
+++ task3-2/task3-2/Program.cs
@@ -0,0 +1,119 @@
+﻿namespace task3_2
+{
+    class Book
+    {
+        string title;
+        string author;
+        string isbn;
+        bool availability;
+
+        public Book(string title, string author, string isbn, bool availability = true)
+        {
+            this.title = title;
+            this.author = author;
+            this.isbn = isbn;
+            this.availability = availability;
+        }
+        public string GetTitle() => this.title;
+        public string GetAuthor() => this.author;
+        public string GetISBN()=>this.isbn;
+        public bool IsAvailable()=> this.availability;
+        public void SetAvailability(bool value)
+        {
+            this.availability = value;
+        }
+
+    }
+    class Library
+    {
+        List<Book> books = new List<Book>();
+        public string AddBook(Book book)
+        {
+            books.Add(book);
+            return ($"Book '{book.GetTitle()}' added successfully!");
+        }
+        public Book SearchBook(string title)
+        {
+            for (int i = 0; i < books.Count; i++)
+            {
+                if (books[i].GetTitle() == title)
+                {
+                    return books[i];
+                }
+            }
+            return null ;
+        }
+        public bool BorrowBook(string title)
+        {
+            for (int i = 0; i < books.Count; i++)
+            {
+                if (books[i].GetTitle() == title)
+                {
+                    if (books[i].IsAvailable() == true)
+                    {
+                        books[i].SetAvailability(false);
+                        Console.WriteLine($"Book {title} borrowed successfully."); 
+                        return true;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Book {title} is currently not available (already borrowed)."); 
+                        return false;
+                    }
+                }
+            }
+            Console.WriteLine($"Book {title} not found in the library."); 
+            return false;
+        }
+        public bool ReturnBook(string title)
+        {
+            for (int i = 0; i < books.Count; i++)
+            {
+                if (books[i].GetTitle() == title)
+                {
+                    if (books[i].IsAvailable() == false)
+                    {
+                        books[i].SetAvailability(true);
+                        Console.WriteLine($"Book {title} returned successfully.");
+                        return true;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Book {title} was not borrowed.");
+                        return false;
+                    }
+                }
+            }
+            Console.WriteLine($"Book {title} not found in the library.");
+            return false;
+        }
+        internal class Program
+        {
+
+            static void Main()
+            {
+
+                Library library = new Library();
+
+                // Adding books to the library
+                library.AddBook(new Book("The Great Gatsby", "F. Scott Fitzgerald", "9780743273565"));
+                library.AddBook(new Book("To Kill a Mockingbird", "Harper Lee", "9780061120084"));
+                library.AddBook(new Book("1984", "George Orwell", "9780451524935"));
+
+                // Searching and borrowing books
+                Console.WriteLine("Searching and borrowing books...");
+                library.BorrowBook("Gatsby");
+                library.BorrowBook("1984");
+                library.BorrowBook("Harry Potter"); // This book is not in the library
+
+                // Returning books
+                Console.WriteLine("\nReturning books...");
+                library.ReturnBook("Gatsby");
+                library.ReturnBook("Harry Potter"); // This book is not borrowed
+
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
